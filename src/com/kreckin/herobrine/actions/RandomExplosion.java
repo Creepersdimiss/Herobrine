deff032482fbfb0daecda34fd72f1af39663fbb5
Krecklow, Nicholas R <owner@nkrecklow.com>
2013-02-16 01:31:16 -0500

    Don't always target the player.

--- src/com/kreckin/herobrine/actions/RandomExplosion.java
+++ src/com/kreckin/herobrine/actions/RandomExplosion.java
@@ -3,6 +3,7 @@ package com.kreckin.herobrine.actions;
 import com.kreckin.herobrine.api.Action;
 import com.kreckin.herobrine.api.ActionResult;
 import com.kreckin.herobrine.api.ActionType;
+import com.kreckin.herobrine.util.Util;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -14,8 +15,8 @@ public class RandomExplosion extends Action {
 
     @Override
     public ActionResult callAction(Player player, Object[] metadata) {
-        player.getWorld().createExplosion(player.getLocation(), 2F);
-        Location loc = player.getLocation();
+        Location loc = Util.getNearbyLocation(player, 3);
+        player.getWorld().createExplosion(loc, 2F);
         return (new ActionResult("Done.", "Location: " + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ()));
     }
 }
--- src/com/kreckin/herobrine/actions/RandomLightning.java
+++ src/com/kreckin/herobrine/actions/RandomLightning.java
@@ -3,6 +3,7 @@ package com.kreckin.herobrine.actions;
 import com.kreckin.herobrine.api.Action;
 import com.kreckin.herobrine.api.ActionResult;
 import com.kreckin.herobrine.api.ActionType;
+import com.kreckin.herobrine.util.Util;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -14,8 +15,8 @@ public class RandomLightning extends Action {
 
     @Override
     public ActionResult callAction(Player player, Object[] metadata) {
-        player.getWorld().strikeLightning(player.getLocation());
-        Location loc = player.getLocation();
+        Location loc = Util.getNearbyLocation(player, 10);
+        player.getWorld().strikeLightning(loc);
         return (new ActionResult("Done.", "Location: " + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ()));
     }
 }
