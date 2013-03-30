657786be49b32aaf9acb3dfec81603b63af5dd04
Krecklow, Nick <hello@kreckin.com>
2013-03-30 11:56:46 -0700

    Use new structure system.

--- src/com/kreckin/herobrine/actions/CreatePyramid.java
+++ src/com/kreckin/herobrine/actions/CreatePyramid.java
@@ -1,11 +1,9 @@
 package com.kreckin.herobrine.actions;
 
 import com.kreckin.herobrine.api.Action;
+import com.kreckin.herobrine.api.Structure;
 import com.kreckin.herobrine.util.Util;
 import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 
 public class CreatePyramid extends Action {
@@ -16,39 +14,10 @@ public class CreatePyramid extends Action {
 
     @Override
     public String callAction(Player player, Object[] metadata) {
-        Block[] blocks = new Block[14];
-        blocks[0] = Util.getNearbyLocation(player, 25).add(0, 2, 0).getBlock();
-        blocks[1] = blocks[0].getRelative(BlockFace.NORTH).getLocation().subtract(0, 1, 0).getBlock();
-        blocks[2] = blocks[0].getRelative(BlockFace.SOUTH).getLocation().subtract(0, 1, 0).getBlock();
-        blocks[3] = blocks[0].getRelative(BlockFace.WEST).getLocation().subtract(0, 1, 0).getBlock();
-        blocks[4] = blocks[0].getRelative(BlockFace.EAST).getLocation().subtract(0, 1, 0).getBlock();
-        blocks[5] = blocks[1].getRelative(BlockFace.NORTH).getLocation().subtract(0, 1, 0).getBlock();
-        blocks[6] = blocks[2].getRelative(BlockFace.SOUTH).getLocation().subtract(0, 1, 0).getBlock();
-        blocks[7] = blocks[3].getRelative(BlockFace.WEST).getLocation().subtract(0, 1, 0).getBlock();
-        blocks[8] = blocks[4].getRelative(BlockFace.EAST).getLocation().subtract(0, 1, 0).getBlock();
-        blocks[9] = blocks[0].getRelative(BlockFace.NORTH_EAST).getLocation().subtract(0, 2, 0).getBlock();
-        blocks[10] = blocks[0].getRelative(BlockFace.SOUTH_EAST).getLocation().subtract(0, 2, 0).getBlock();
-        blocks[11] = blocks[0].getRelative(BlockFace.SOUTH_WEST).getLocation().subtract(0, 2, 0).getBlock();
-        blocks[12] = blocks[0].getRelative(BlockFace.NORTH_WEST).getLocation().subtract(0, 2, 0).getBlock();
-        blocks[13] = blocks[0].getLocation().subtract(0, 1, 0).getBlock();
-        for (int index = 0; index < blocks.length; index++) {
-            Block block = blocks[index];
-            if (index >= 5 && index <= 12) {
-                if (Util.isSolid(block.getLocation().subtract(0, 1, 0).getBlock()) || player.getLocation().distance(block.getLocation()) <= 1F) {
-                    return "Failed, could not find a proper location!";
-                }
-            }
+        Location loc = Util.getNearbyLocation(player, 10);
+        if (!Structure.loadStructure(loc, "/com/kreckin/herobrine/structures/Pyramid.yml").createStructure()) {
+            return "Failed, could not find a proper location!";
         }
-        for (int index = 0; index < blocks.length; index++) {
-            Block block = blocks[index];
-            if (index == 13) {
-                block.setType(Material.NETHERRACK);
-                blocks[0].setType(Material.FIRE);
-            } else {
-                block.setType(Material.SANDSTONE);
-            }
-        }
-        Location loc = blocks[0].getLocation();
         return ("Location: " + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ());
     }
 }
--- src/com/kreckin/herobrine/actions/CreateRingOfFire.java
+++ src/com/kreckin/herobrine/actions/CreateRingOfFire.java
@@ -1,11 +1,7 @@
 package com.kreckin.herobrine.actions;
 
 import com.kreckin.herobrine.api.Action;
-import com.kreckin.herobrine.util.Util;
-import java.util.ArrayList;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
+import com.kreckin.herobrine.api.Structure;
 import org.bukkit.entity.Player;
 
 public class CreateRingOfFire extends Action {
@@ -16,24 +12,9 @@ public class CreateRingOfFire extends Action {
 
     @Override
     public String callAction(Player player, Object[] metadata) {
-        Block[] blocks = new Block[8];
-        blocks[0] = player.getLocation().add(2, 0, 0).getBlock();
-        blocks[1] = player.getLocation().add(0, 0, 2).getBlock();
-        blocks[2] = player.getLocation().add(0, 0, -2).getBlock();
-        blocks[3] = player.getLocation().add(-2, 0, 0).getBlock();
-        blocks[4] = player.getLocation().add(1, 0, 1).getBlock();
-        blocks[5] = player.getLocation().add(1, 0, -1).getBlock();
-        blocks[6] = player.getLocation().add(-1, 0, 1).getBlock();
-        blocks[7] = player.getLocation().add(-1, 0, -1).getBlock();
-        for (Block block : blocks) {
-            if (!Util.isValid(block)) {
-                return "Failed, could not find a proper location!";
-            }
+        if (!Structure.loadStructure(player.getLocation(), "/com/kreckin/herobrine/structures/RingOfFire.yml").createStructure()) {
+            return "Failed, could not find a proper location!";
         }
-        for (Block block : blocks) {
-            block.setType(Material.FIRE);
-        }
-        Location loc = player.getLocation();
-        return ("Location: " + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ());
+        return ("Location: " + player.getLocation().getBlockX() + ", " + player.getLocation().getBlockY() + ", " + player.getLocation().getBlockZ());
     }
 }
