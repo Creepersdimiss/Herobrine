2b235a81aa1c1961e169a4e3975ca60b933662c4
Krecklow, Nick <hello@kreckin.com>
2013-04-19 11:40:58 -0500

    Properly indent.

--- src/com/kreckin/herobrine/api/ChatManager.java
+++ src/com/kreckin/herobrine/api/ChatManager.java
@@ -5,25 +5,25 @@ import org.bukkit.entity.Player;
 
 public class ChatManager {
 
-  private final ArrayList<String> usernames;
+    private final ArrayList<String> usernames;
   
-  public ChatManager() {
-    this.usernames = new ArrayList<String>();
-  }
-  
-  public void addPlayer(Player player) {
-    if (!this.usernames.contains(player.getName())) {
-      this.usernames.add(player.getName());
+    public ChatManager() {
+        this.usernames = new ArrayList<String>();
     }
-  }
   
-  public void removePlayer(Player player) {
-    if (this.usernames.contains(player.getName())) {
-      this.usernames.remove(player.getName());
+    public void addPlayer(Player player) {
+        if (!this.usernames.contains(player.getName())) {
+            this.usernames.add(player.getName());
+        }
     }
-  }
   
-  public ArrayList<String> getUsernames() {
-    return this.usernames;
-  }
+    public void removePlayer(Player player) {
+        if (this.usernames.contains(player.getName())) {
+            this.usernames.remove(player.getName());
+        }
+    }
+    
+    public ArrayList<String> getUsernames() {
+        return this.usernames;
+    }
 }
