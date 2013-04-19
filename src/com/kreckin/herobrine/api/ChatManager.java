3a5fcfc393022e0d19384b5dda957e6235e2aa23
Krecklow, Nick <hello@kreckin.com>
2013-04-19 11:40:20 -0500

    Finish the stub.

--- src/com/kreckin/herobrine/api/ChatManager.java
+++ src/com/kreckin/herobrine/api/ChatManager.java
@@ -1,5 +1,29 @@
 package com.kreckin.herobrine.api;
 
+import java.util.ArrayList;
+import org.bukkit.entity.Player;
+
 public class ChatManager {
 
+  private final ArrayList<String> usernames;
+  
+  public ChatManager() {
+    this.usernames = new ArrayList<String>();
+  }
+  
+  public void addPlayer(Player player) {
+    if (!this.usernames.contains(player.getName())) {
+      this.usernames.add(player.getName());
+    }
+  }
+  
+  public void removePlayer(Player player) {
+    if (this.usernames.contains(player.getName())) {
+      this.usernames.remove(player.getName());
+    }
+  }
+  
+  public ArrayList<String> getUsernames() {
+    return this.usernames;
+  }
 }
